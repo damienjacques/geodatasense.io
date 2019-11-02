6416882e33415aaf09a8d90e1c291b427b0300d3
damienjacques <damienjacques1989@gmail.com>
2019-11-02 14:13:22 +0100

    spelling

--- js/script.js
+++ js/script.js
@@ -136,3 +136,21 @@ $('#lang-switch').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
                         
+
+// fallback function for Font Awesome
+function ensureCssFileInclusion(cssFileToCheck) {
+    var styleSheets = document.styleSheets;
+    for (var i = 0, max = styleSheets.length; i < max; i++) {
+        if (styleSheets[i].href == cssFileToCheck) {
+            return;
+        }
+    }
+    // because no matching stylesheets were found, we will add a new HTML link element to the HEAD section of the page.
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "https://downtimmonkey.com/path-xxx/font-awesome/css/font-awesome.min.css";
+    document.getElementsByTagName("head")[0].appendChild(link);
+}
+
+//call function
+ensureCssFileInclusion("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
