668a78905dc06bc187ae6cfa2a6c95fe7bac6061
damienjacques <damienjacques1989@gmail.com>
2019-11-05 12:08:21 +0100

    add CTA and review content

--- js/script.js
+++ js/script.js
@@ -94,8 +94,8 @@ owl.owlCarousel({
 $(document).ready(function(){
   
   // By default
-  $('.en_lang').addClass("active-lang");
-  $('#lang-switch .en').addClass("active-flag");
+  $('.fr_lang').addClass("active-lang");
+  $('#lang-switch .fr').addClass("active-flag");
   
   // Function switch
   $(function() {
